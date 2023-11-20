4773ad71e899edfc1087e1c9f47fca2f3e71aeee
Joshua Macatunao <108927748+pi-josh@users.noreply.github.com>
2023-11-21 07:29:53 +0800

    Solved the day 02 challenge time

---
+++ submissions/challenge-time/day-02-challenge-time.py
@@ -0,0 +1,10 @@
+# Prompting the user for inputs
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+# Calculating the sum
+sum = num1 + num2 + num3
+
+# Displaying the output
+print(f"Sum: {sum}")
