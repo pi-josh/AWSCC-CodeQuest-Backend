cafe510bdee9fae5b9a08963656a1f0c593e15f7
Joshua Macatunao <108927748+pi-josh@users.noreply.github.com>
2023-11-20 14:26:05 +0800

    Assigned values to the variables

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "Joshua C. Macatunao"
+age = 20
+isMale = True
+department = "Department of Software Development"
 
 print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
