d257a77b9d5654d1849414c056f016cdc97e8e28
Joshua Macatunao <108927748+pi-josh@users.noreply.github.com>
2023-11-21 07:29:12 +0800

    Moved the file

--- submissions/day-01-challenge-time.py
+++
@@ -1,5 +0,0 @@
-name = "Joshua"
-department = "Department of Software Development"
-about_me = "I am passionate to learn what lies behind and beyond backend development"
-
-print(f"Hello, I am {name}. I love {department} and I love being in the Backend even more!\nOne thing about me is that {about_me}.")
