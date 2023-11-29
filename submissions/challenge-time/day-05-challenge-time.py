1886810ac3c2c15b093642af895d80fc11407f50
Joshua Macatunao <108927748+pi-josh@users.noreply.github.com>
2023-11-29 08:33:33 +0800

    Improved the code

--- submissions/challenge-time/day-05-challenge-time.py
+++ submissions/challenge-time/day-05-challenge-time.py
@@ -12,12 +12,13 @@ choice = ["rock", "paper", "scissors"]
 # Prompting the user for their choices
 player1 = input("Player 1: ").lower()
 player2 = random.choice(choice)
+print(f"Player 1: {player1}")
 print(f"Player 2: {player2}")
 
 # Evaluating who wins
 if (player1 == "rock" and player2 == "scissors") or (player1 == "scissors" and player2 == "paper") or (player1 == "paper" and player2 == "rock"):
     print("Player 1 Wins!")
-elif player2 == "rock" and player1 == "scissors" or (player2 == "scissors" and player1 == "paper") or (player2 == "paper" and player1 == "rock"):
-    print("Player 2 Wins!")
-else:
+elif player1 == player2:
     print("Draw!")
+else:
+    print("Player 2 Wins!")
