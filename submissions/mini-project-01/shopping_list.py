4606624c07cc2ffdd95a84c53fe5a5e09d8171af
Joshua Macatunao <108927748+pi-josh@users.noreply.github.com>
2023-12-01 09:10:48 +0800

    Fixed bugs

--- submissions/mini-project-01/shopping_list.py
+++ submissions/mini-project-01/shopping_list.py
@@ -1,7 +1,7 @@
 # Mini Project: Basic Shopping List Program in Python
 
-# Reading the shopping list text file
-shopping_list = open('shopping_list.txt', 'r')
+# Variable to store the items
+shopping_list = []
 
 def main():
     # Signboard
@@ -47,16 +47,35 @@ def main():
            ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝                                          
           """)
             print("\t\t\t\tPlease come again!!!\n\n")
-            shopping_list.close()
         else:
             print("\n\n\t\t\tInvalid input! Please try again...\n\n")
 
+
+def reading_file():
+    global shopping_list
+    shopping_list = []
+    with open('shopping_list.txt', 'r') as file:
+        items = file.readlines()
+        for item in items:
+            item = item.strip()
+            shopping_list.append(item)
+            
+
+def writing_file(product):
+    with open('shopping_list.txt', 'w') as file:
+        for item in shopping_list:
+            if product == item:
+                print(f"\n\n\t\t\t\t{item} has been removed from your shopping list.\n\n")
+                continue
+            else:
+                file.write(item + "\n")
+            
 
 def add_items():
     # Prompt the user for the item to be added
     item = input("Enter the item you want to add: ").lower().capitalize()
     with open('shopping_list.txt', 'a') as file:
-        if not item in shopping_list.readlines():
+        if not item in shopping_list:
             file.write(item + "\n")    
             print(f"\n\n\t\t\t\t{item} has been added to your shopping list.\n\n")
         else:
@@ -65,8 +84,9 @@ def add_items():
     
 def view_items():
     # Show the user the list
+    reading_file()
     print("\n\n\t\t\t\tYour shopping list:")
-    for item in shopping_list.readlines():
+    for item in shopping_list:
         print("\t\t\t\t" + item)
     print("\n\n")
 
@@ -74,13 +94,8 @@ def view_items():
 def remove_items():
     # Prompt the user for the item to be removed
     item = input("Enter the item you want to remove: ").lower().capitalize()
-    if item in shopping_list.readlines():
-        with open('shopping_list.txt', 'w') as file:  
-            for product in shopping_list.readlines():
-                if item != product:
-                    file.writelines(product)
-                else:
-                    print(f"\n\n\t\t\t\t{item} has been removed from your shopping list.\n\n")
+    if item in shopping_list:
+        writing_file(item)
     else:
         print(f"\n\n\t\t\t\t{item} is not in your shopping list.\n\n")
 
