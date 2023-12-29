05adfea474716db19a9576b9096938ba827d58b6
Joshua Macatunao <108927748+pi-josh@users.noreply.github.com>
2023-12-29 17:50:23 +0800

    Optimized the code for readability

--- submissions/mini-project-01/shopping_list.py
+++ submissions/mini-project-01/shopping_list.py
@@ -4,53 +4,11 @@
 shopping_list = []
 
 def main():
-    # Signboard
-    print("""
-        ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
-        ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
-        ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
-        ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
-        ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
-         ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
-                                                              
-          """)
-    print("\t\t\t\tEnjoy shopping!!!\n\n")
-    
-    quit = False
-    while not quit:
-        # Show the menu in the prompt
-        print("""
-          Menu.
-            1. Add item to the shopping list
-            2. View shopping list
-            3. Remove item from the shopping list
-            4. Quit
-          """)
-    
-        # Prompt the user for their choice
-        choice = input("Enter the number of your choice: ")
-        
-        if choice == '1':
-            add_items()
-        elif choice == '2':
-            view_items()
-        elif choice == '3':
-            remove_items()
-        elif choice == '4':
-            quit = True
-            print("""
-        ████████╗██╗  ██╗ █████╗ ███╗   ██╗██╗  ██╗    ██╗   ██╗ ██████╗ ██╗   ██╗
-        ╚══██╔══╝██║  ██║██╔══██╗████╗  ██║██║ ██╔╝    ╚██╗ ██╔╝██╔═══██╗██║   ██║
-           ██║   ███████║███████║██╔██╗ ██║█████╔╝      ╚████╔╝ ██║   ██║██║   ██║
-           ██║   ██╔══██║██╔══██║██║╚██╗██║██╔═██╗       ╚██╔╝  ██║   ██║██║   ██║
-           ██║   ██║  ██║██║  ██║██║ ╚████║██║  ██╗       ██║   ╚██████╔╝╚██████╔╝
-           ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝                                          
-          """)
-            print("\t\t\t\tPlease come again!!!\n\n")
-        else:
-            print("\n\n\t\t\tInvalid input! Please try again...\n\n")
+    welcome()
+    menu()
+    thank_you()
 
-
+# Function for reading and writing file to always update the shopping list global variable.
 def reading_file():
     global shopping_list
     shopping_list = []
@@ -98,6 +56,60 @@ def remove_items():
         writing_file(item)
     else:
         print(f"\n\n\t\t\t\t{item} is not in your shopping list.\n\n")
+        
+
+# Function to show the menu to the user
+def menu():
+    while True:
+        # Show the choices on the prompt
+        print("""
+          Menu.
+            1. Add item to the shopping list
+            2. View shopping list
+            3. Remove item from the shopping list
+            4. Quit
+          """)
+    
+        # Prompt the user for their choice
+        choice = input("Enter the number of your choice: ")
+        
+        if choice == '1':
+            add_items()
+        elif choice == '2':
+            view_items()
+        elif choice == '3':
+            remove_items()
+        elif choice == '4':
+            return
+        else:
+            print("\n\n\t\t\tInvalid input! Please try again...\n\n")
+
+
+# To welcome the customer
+def welcome():
+    print("""
+        ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+        ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+        ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
+        ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+        ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+         ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+                                                              
+          """)
+    print("\t\t\t\tEnjoy shopping!!!\n\n")
+
+# To thank the customer for using the service
+def thank_you():
+    print("""
+        ████████╗██╗  ██╗ █████╗ ███╗   ██╗██╗  ██╗    ██╗   ██╗ ██████╗ ██╗   ██╗
+        ╚══██╔══╝██║  ██║██╔══██╗████╗  ██║██║ ██╔╝    ╚██╗ ██╔╝██╔═══██╗██║   ██║
+           ██║   ███████║███████║██╔██╗ ██║█████╔╝      ╚████╔╝ ██║   ██║██║   ██║
+           ██║   ██╔══██║██╔══██║██║╚██╗██║██╔═██╗       ╚██╔╝  ██║   ██║██║   ██║
+           ██║   ██║  ██║██║  ██║██║ ╚████║██║  ██╗       ██║   ╚██████╔╝╚██████╔╝
+           ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝                                          
+          """)
+    print("\t\t\t\tPlease come again!!!\n\n")
+
 
 # Call the main to start the program
 main()
