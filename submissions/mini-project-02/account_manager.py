53d05749ef99f449fba46c63e8b177cfab4bf215
pi-josh <joshua.macatunao007@gmail.com>
2024-03-24 13:26:48 +0800

    Added the template for the functionalities of the account manage

---
+++ submissions/mini-project-02/account_manager.py
@@ -0,0 +1,20 @@
+import json
+
+def add_account():
+    pass
+
+
+def view_account():
+    pass
+
+
+def search_account():
+    pass
+
+
+def update_account():
+    pass
+
+
+def delete_account():
+    pass
